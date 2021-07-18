48d12fa990ee86e82a2a5627f855a531f7befc0e
siayouyang <67354194+siayouyang@users.noreply.github.com>
2021-07-19 02:51:19 +0800

    Delete removerRNAlocus.sh

--- removerRNAlocus.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#author:siayouyang
-#filter out repetitive rRNA locus (chrXII:451275-469084)
-#input format must be SAM
-#usage ./removerRNAlocus.sh input.sam > output.sam
-
-cat $1|awk '{if($3!~"chrXII" || $4<451275 && $4>469084 && $8<451275 && $8>469084 || $1~"^@")print}'
